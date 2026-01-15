4804c0287343aeca8ec8381747c7df8f36c969c5
Sujal Choudhary <sujal9821914131@gmail.com>
2026-01-16 00:02:55 +0530

    Initial commit: Advanced Calculator

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const value = button.innerText;
+    if (value === "AC") {
+      display.value = "";
+    } 
+    else if (value === "⌫") {
+      display.value = display.value.slice(0, -1);
+    } 
+    else if (value === "=") {
+      try {
+        display.value = eval(
+          display.value.replace("×", "*").replace("÷", "/").replace("−", "-")
+        );
+      } catch {
+        display.value = "Error";
+      }
+    } 
+    else {
+      display.value += value;
+    }
+  });
+});
